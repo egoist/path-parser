f4be7730b183d02fe09889552f12951641211bf9
EGOIST <0x142857@gmail.com>
2021-04-03 21:33:27 +0800

    fix: export PathParserOptions and tokensToParser

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { comparePathParserScore } from './pathParserRanker'
 
 export type { PathParser } from './pathParserRanker'
 
-export { tokenizePath }
+export { tokenizePath, tokensToParser, PathParserOptions }
 
 export const createParser = (path: string, options?: PathParserOptions) => {
   return tokensToParser(tokenizePath(path), options)
